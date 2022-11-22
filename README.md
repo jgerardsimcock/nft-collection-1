a549ed56ccbccc14792259b71469062f329c4d4d
Justin Simcock <justinsimcock@Justins-MacBook-Pro.local>
2022-11-22 12:06:10 -0500

    tests passing but I don't really understand

--- README.md
+++ README.md
@@ -5,6 +5,15 @@ What do I want this contract to do?
 
 Receive calls to mint tokens everyday as I push new images to Arweave
 
+What is the MVP of this?
+
+When I mint I pass in a TokenURI string
+Then we mint a token that points to the asset at the URI
+
+https://r5sr6zbjdnqvvrin4z56zqzuwlo7coc7m2dvr6tpzuhx76rzgkra.arweave.net/j2UfZCkbYVrFDeZ77MM0st3xOF9mh1j6b80Pf_o5MqI
+
+https://jxudqnf3isxb5oqief4ywuc425tnxj7icn7ssdtuvsio73gofmba.arweave.net/Teg4NLtErh66CCF5i1Bc12bbp-gTfykOdKyQ7-zOKwI
+
 
 
 Here is how this has to work
@@ -36,3 +45,13 @@ Foundry notes
 
 You need to initialize a foundry repo
 You need to install the oz contracts
+
+
+Tools used in this application
+
+[Arweave Go SDK](https://github.com/everFinance/goar)
+[Foundry](https://github.com/foundry-rs)
+[Solidity]
+[Stable Diffusion](https://github.com/CompVis/stable-diffusion)
+
+
--- src/Infinessence.sol
+++ src/Infinessence.sol
@@ -7,13 +7,20 @@ import "lib/openzeppelin-contracts/contracts/security/Pausable.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+// import "lib/openzeppelin-contracts/contracts/utils/Strings.sol";
 
-contract Infinessence is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
+contract Infinessence is ERC721URIStorage, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
     constructor() ERC721("Infinessence", "INF") {}
+
+    string uri;
+
+    function setURI(string memory _uri) public onlyOwner {
+        uri = _uri;
+    }
 
     function pause() public onlyOwner {
         _pause();
@@ -23,12 +30,37 @@ contract Infinessence is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burn
         _unpause();
     }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+
+    function safeMint(address to, string memory _uri) 
+        
+        public onlyOwner 
+        returns (uint256) 
+
+    {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+        setURI(_uri);
+        tokenURI(tokenId);
+        // _setTokenURI(tokenId, uri);
+        return tokenId;
+
     }
+
+
+    function _baseURI() internal view override returns (string memory) {
+        return uri;
+    }
+
+    function tokenURI(uint tokenId)
+        public 
+        view
+        override(ERC721URIStorage, ERC721)
+        returns (string memory)
+        {
+            return string(abi.encodePacked(_baseURI()));
+        }
+
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
@@ -44,12 +76,6 @@ contract Infinessence is ERC721, ERC721URIStorage, Pausable, Ownable, ERC721Burn
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
-        public
-        view
-        override(ERC721, ERC721URIStorage)
-        returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
+
+    // function transferTo(){}
 }
